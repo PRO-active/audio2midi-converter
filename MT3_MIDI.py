75e49b3fa91e5daf0f2063a39488499c7e8a9a37
banana <hayamatsu8810@gmail.com>
2023-12-13 14:41:59 +0900

    [ADD] モデルの選択を可能に

--- MT3_MIDI.py
+++ MT3_MIDI.py
@@ -5,7 +5,10 @@ from pydub import AudioSegment
 import io
 
 # Load MT3 model
-MODEL = "mt3"
+MODEL_OPTIONS = {"ピアノ採譜モデル": "ismir2021", "複数楽器採譜モデル": "mt3"}
+selected_model = st.selectbox("モデルを選択してください", list(MODEL_OPTIONS.keys()))
+
+MODEL = MODEL_OPTIONS[selected_model]
 CHECKPOINT_PATH = f'/app/checkpoints/{MODEL}/'
 inference_model = InferenceModel(CHECKPOINT_PATH, MODEL)
 
@@ -18,8 +21,8 @@ def upload_audio(file):
     data = file.read()
     file_extension = file.name.split('.')[-1]
     if file_extension.lower() == 'mp3':
-        wav_data, sample_rate = mp3_to_wav(data)
-        audio_samples = note_seq.audio_io.wav_data_to_samples_librosa(wav_data, sample_rate=sample_rate)
+        wav_data = mp3_to_wav(data)
+        audio_samples = note_seq.audio_io.wav_data_to_samples_librosa(wav_data, sample_rate=inference_model.SAMPLE_RATE)
     else:
         audio_samples = note_seq.audio_io.wav_data_to_samples_librosa(data, sample_rate=inference_model.SAMPLE_RATE)
     return audio_samples
@@ -27,8 +30,8 @@ def upload_audio(file):
 def mp3_to_wav(mp3_data):
     audio = AudioSegment.from_mp3(io.BytesIO(mp3_data))
     wav_data = audio.raw_data
-    sample_rate = audio.frame_rate
-    return wav_data, sample_rate
+    # sample_rate = audio.frame_rate
+    return wav_data
 
 if uploaded_file is not None:
     st.audio(uploaded_file, format='audio/wav')
